98b0ecc0c33ad5d5416d617683bb09dc89f439b5
soumya4569 <soumya.raja04569@gmail.com>
2026-08-01 17:09:01 +0530

    shit

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function createWindow() {
     }
   });
 
-  mainWindow.loadURL('https://magixx-frontend.vercel.app/');
+  mainWindow.loadURL('https://https://magixx-cafe.vercel.app/');
 }
 
 app.whenReady().then(() => {

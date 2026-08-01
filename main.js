5af01998e6c81c00660cb55ba2b9ec96a01d19da
soumya4569 <soumya.raja04569@gmail.com>
2026-08-01 17:49:58 +0530

    Mar jaunga

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ function createWindow() {
     }
   });
 
-  mainWindow.loadURL('https://magixx-frontend.vercel.app/');
+  mainWindow.loadURL('https://magixx-cafe.vercel.app/');
 }
 
 app.whenReady().then(() => {
